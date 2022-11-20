fe6cea8ceaf72b5a06e4df3be6e1b1ee5b5c6d0c
Giacomomangelli <giacomomsidp@gmail.com>
2022-11-20 18:10:26 +0100

    making the side bar app compatible with flexible layout

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -18,156 +18,153 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SideMenu(
-            controller: page,
-            style: SideMenuStyle(
-              // showTooltip: false,
-              displayMode: SideMenuDisplayMode.auto,
-              hoverColor: Colors.blue[100],
-              selectedColor: Colors.lightBlue,
-              selectedTitleTextStyle: const TextStyle(color: Colors.white),
-              selectedIconColor: Colors.white,
-              // decoration: BoxDecoration(
-              //   borderRadius: BorderRadius.all(Radius.circular(10)),
-              // ),
-              // backgroundColor: Colors.blueGrey[700]
-            ),
-            title: Column(
-              children: [
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    maxHeight: 150,
-                    maxWidth: 150,
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
+        ),
+        body: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Flexible(
+            flex: 1,
+            child: SideMenu(
+              controller: page,
+              style: SideMenuStyle(
+                // showTooltip: false,
+                displayMode: SideMenuDisplayMode.auto,
+                hoverColor: Colors.blue[100],
+                selectedColor: Colors.lightBlue,
+                selectedTitleTextStyle: const TextStyle(color: Colors.white),
+                selectedIconColor: Colors.white,
+                // decoration: BoxDecoration(
+                //   borderRadius: BorderRadius.all(Radius.circular(10)),
+                // ),
+                // backgroundColor: Colors.blueGrey[700]
+              ),
+              title: Column(
+                children: [
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxHeight: 150,
+                      maxWidth: 150,
+                    ),
+                    child: Image.asset(
+                      'images/sidp_logo_splash.png',
+                    ),
                   ),
-                  child: Image.asset(
-                    'images/sidp_logo_splash.png',
+                  const Divider(
+                    indent: 8.0,
+                    endIndent: 8.0,
                   ),
+                ],
+              ),
+              footer: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'mohada',
+                  style: TextStyle(fontSize: 15),
                 ),
-                const Divider(
-                  indent: 8.0,
-                  endIndent: 8.0,
+              ),
+              items: [
+                SideMenuItem(
+                  priority: 0,
+                  title: 'Dashboard',
+                  onTap: () {
+                    page.jumpToPage(0);
+                  },
+                  icon: const Icon(Icons.home),
+                  badgeContent: const Text(
+                    '3',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  tooltipContent: "This is a tooltip for Dashboard item",
+                ),
+                SideMenuItem(
+                  priority: 1,
+                  title: 'Users',
+                  onTap: () {
+                    page.jumpToPage(1);
+                  },
+                  icon: const Icon(Icons.supervisor_account),
+                ),
+                SideMenuItem(
+                  priority: 2,
+                  title: 'Files',
+                  onTap: () {
+                    page.jumpToPage(2);
+                  },
+                  icon: const Icon(Icons.file_copy_rounded),
+                  trailing: Container(
+                      decoration: const BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.all(Radius.circular(6))),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 6.0, vertical: 3),
+                        child: Text(
+                          'New',
+                          style:
+                              TextStyle(fontSize: 11, color: Colors.grey[800]),
+                        ),
+                      )),
+                ),
+                SideMenuItem(
+                  priority: 3,
+                  title: 'Download',
+                  onTap: () {
+                    page.jumpToPage(3);
+                  },
+                  icon: const Icon(Icons.download),
+                ),
+                SideMenuItem(
+                  priority: 4,
+                  title: 'Settings',
+                  onTap: () {
+                    page.jumpToPage(4);
+                  },
+                  icon: const Icon(Icons.settings),
+                ),
+                // SideMenuItem(
+                //   priority: 5,
+                //   onTap: () {
+                //     page.jumpToPage(5);
+                //   },
+                //   icon: const Icon(Icons.image_rounded),
+                // ),
+                // SideMenuItem(
+                //   priority: 6,
+                //   title: 'Only Title',
+                //   onTap: () {
+                //     page.jumpToPage(6);
+                //   },
+                // ),
+                const SideMenuItem(
+                  priority: 7,
+                  title: 'Exit',
+                  icon: Icon(Icons.exit_to_app),
                 ),
               ],
             ),
-            footer: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'mohada',
-                style: TextStyle(fontSize: 15),
-              ),
-            ),
-            items: [
-              SideMenuItem(
-                priority: 0,
-                title: 'Dashboard',
-                onTap: () {
-                  page.jumpToPage(0);
-                },
-                icon: const Icon(Icons.home),
-                badgeContent: const Text(
-                  '3',
-                  style: TextStyle(color: Colors.white),
-                ),
-                tooltipContent: "This is a tooltip for Dashboard item",
-              ),
-              SideMenuItem(
-                priority: 1,
-                title: 'Users',
-                onTap: () {
-                  page.jumpToPage(1);
-                },
-                icon: const Icon(Icons.supervisor_account),
-              ),
-              SideMenuItem(
-                priority: 2,
-                title: 'Files',
-                onTap: () {
-                  page.jumpToPage(2);
-                },
-                icon: const Icon(Icons.file_copy_rounded),
-                trailing: Container(
-                    decoration: const BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.all(Radius.circular(6))),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 6.0, vertical: 3),
-                      child: Text(
-                        'New',
-                        style: TextStyle(fontSize: 11, color: Colors.grey[800]),
-                      ),
-                    )),
-              ),
-              SideMenuItem(
-                priority: 3,
-                title: 'Download',
-                onTap: () {
-                  page.jumpToPage(3);
-                },
-                icon: const Icon(Icons.download),
-              ),
-              SideMenuItem(
-                priority: 4,
-                title: 'Settings',
-                onTap: () {
-                  page.jumpToPage(4);
-                },
-                icon: const Icon(Icons.settings),
-              ),
-              // SideMenuItem(
-              //   priority: 5,
-              //   onTap: () {
-              //     page.jumpToPage(5);
-              //   },
-              //   icon: const Icon(Icons.image_rounded),
-              // ),
-              // SideMenuItem(
-              //   priority: 6,
-              //   title: 'Only Title',
-              //   onTap: () {
-              //     page.jumpToPage(6);
-              //   },
-              // ),
-              const SideMenuItem(
-                priority: 7,
-                title: 'Exit',
-                icon: Icon(Icons.exit_to_app),
-              ),
-            ],
           ),
-       LayoutDisplayer(
-           containerChild: SizedBox(
-              width: MediaQuery.of(context).size.width,
-               child: ListView(
-                    padding: const EdgeInsets.all(8),
-                    children: const <Widget>[
-                      Spacer(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      SubjectCard(),
-                      Spacer()
-                    ]
-               )
-           )
-       )
-    ])
-    );
+          Flexible(
+              flex: 2,
+              child: ListView(
+                  padding: const EdgeInsets.all(8),
+                  children: const <Widget>[
+                    Spacer(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    SubjectCard(),
+                    Spacer()
+                  ]))
+        ]));
     //             Container(
     //               color: Colors.white,
     //               child: const Center(
